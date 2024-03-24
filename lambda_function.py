e674b85452f69203c3ad317b1580bace7cd658e8
hp2p <jaeman71.kim@samsung.com>
2024-03-24 17:25:55 +0900

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -18,41 +18,8 @@ def get_api_key():
     openai_api_key = json.load(response['Payload'])['body']['api_key']
     return openai_api_key
     
-    
-def lambda_handler(event, context):
-    gmt_time = time.gmtime()
-
-    now = time.strftime('%a, %d %b %Y %H:%M:%S +0000', gmt_time)
-    word = event['word']
-    
-    response = table.get_item(Key={'word': word})
-    
-    if 'Item' in response:
-        item = response['Item']
-
-        return {
-            'statusCode': 200,
-            'body': f'[{json.dumps(item)}]'
-        }
-        
-    '''else:
-        try:
-            table.put_item(
-                Item={
-                'word': word,
-                'usages': ['usage1', 'usage2', 'usage3'],
-                "related_information": "관련 정보 1, 2, 3",
-                'meanings': '뜻1, 뜻2'
-                },
-                ConditionExpression='attribute_not_exists(word)'
-            )
-        except botocore.exceptions.ClientError as e:
-            if e.response['Error']['Code'] != 'ConditionalCheckFailedException':
-                raise
-'''
+def query_llm(word):
     client = OpenAI( api_key=get_api_key() )
-    
-    word = word.lower()
     user_prompt = f'''I want to memorize the meanings and usages of the vocabulary "{word}".
 Create a json string including 3 short usage samples including "{word}", 
 related information helpful to memorize "{word}" in Korean, 
@@ -69,13 +36,12 @@ The json should be like:
   "meanings": "하인, 종"
    }}'''
    
-   
     completion = client.chat.completions.create(
         model='gpt-3.5-turbo',
         messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": user_prompt},
-  ],
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": user_prompt},
+        ],
         temperature=0,
         max_tokens=256,
         top_p=1,
@@ -84,28 +50,42 @@ The json should be like:
         )
     
     response = completion.choices[0].message.content
-    print(f'2    response = {response}')
-    item = json.loads(response)
-    print(f'3    item = {item}')
+    return json.loads(response)
     
+    
+def lambda_handler(event, context):
+    gmt_time = time.gmtime()
+
+    now = time.strftime('%a, %d %b %Y %H:%M:%S +0000', gmt_time)
+    word = event['word']
+    
+    response = table.get_item(Key={'word': word})
+    
+    if 'Item' in response:
+        item = response['Item']
+        return {
+            'statusCode': 200,
+            'body': f'[{json.dumps(item)}]'
+        }
+
+    item = query_llm(word.lower())
+
     try:
-            table.put_item(
-                Item={
+        table.put_item(
+            Item={
                 'word': item['word'],
                 'usages': item['usages'],
                 "related_information": item['related_information'],
                 'meanings': item['meanings']
-                },
-                ConditionExpression='attribute_not_exists(word)'
-            )
+            },
+            ConditionExpression='attribute_not_exists(word)'
+        )
     except botocore.exceptions.ClientError as e:
-            if e.response['Error']['Code'] != 'ConditionalCheckFailedException':
-                raise
-
+        if e.response['Error']['Code'] != 'ConditionalCheckFailedException':
+            raise
 
     return {
         'statusCode': 200,
-        #'body': json.dumps(f'[{response}]')
         'body': f'[{response}]'
     }
     
